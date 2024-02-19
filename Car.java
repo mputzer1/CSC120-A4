46fde68818de9891ce44930b7cb29d3fbf50b674
Maura <mputzer@smith.edu>
2024-02-19 10:50:02 -0500

    Beginning of Car.java

--- Car.java
+++ Car.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+
 public class Car {
+    private int capacity;
+    private ArrayList<String> passengers = new ArrayList<String>();
+
+    public Car(int capacity) {
+        this.capacity = capacity;
+        passengers.ensureCapacity(this.capacity);   
+    }
+
+    public int getCapcity() {
+        return this.capacity;
+    }
+
+    public int seatsRemaining() {
+        
+    }
+
 
 }
+
+
