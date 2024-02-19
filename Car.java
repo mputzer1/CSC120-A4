e3a5666e19f5e95c6b435760cef902f7f2c6da99
Maura <mputzer@smith.edu>
2024-02-19 13:22:19 -0500

    Car.java rough draft

--- Car.java
+++ Car.java
@@ -1,23 +1,52 @@
 import java.util.ArrayList;
+import javax.net.ssl.TrustManager;
+import javax.swing.plaf.TreeUI;
 
 public class Car {
     private int capacity;
     private ArrayList<String> passengers = new ArrayList<String>();
 
-    public Car(int capacity) {
+    private Car(int capacity) {
         this.capacity = capacity;
         passengers.ensureCapacity(this.capacity);   
     }
 
-    public int getCapcity() {
+    private int getCapacity() {
         return this.capacity;
     }
 
-    public int seatsRemaining() {
-        
+    private int seatsRemaining() {
+        int arraysize = passengers.size();
+        int remainingseats = this.capacity - arraysize;
+        return remainingseats;
     }
 
+    private boolean addPassenger(String Passenger) {
+        if (seatsRemaining() > 0) {
+            passengers.add(Passenger);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    private boolean removePassenger(String Passenger) {
+        if (passengers.contains(Passenger)) {
+            passengers.remove(Passenger);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    private void printManifest() {
+        System.out.println(passengers);
+    }
+    
+    
+    }
 
-}
 
 
